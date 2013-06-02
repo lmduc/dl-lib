18c1cf4948031bb9b72de3cbb64d0f11d6126074
duc.le <leminhducktvn@gmail.com>
2013-06-02 23:18:52 +0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DlLib::Application.config.secret_token = 'ca8f8b38ccdffc217042e78cb360c58d7238e4d6849d0c1d05841b061b2b700564ab1ed739dc86cd64a48239abd329ba5b07a4e2a544624c2a0d72d02d1e7d64'
